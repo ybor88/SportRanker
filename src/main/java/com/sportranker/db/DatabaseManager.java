f5ffec3882fd857e8d18cea7b73e284b7f0f09e0
DIR\roberto.di.flumeri <paolo.fusi@consulenti.gruppohera.it>
2025-06-09 15:23:13 +0200

    fix db properties

--- src/main/java/com/sportranker/db/DatabaseManager.java
+++ src/main/java/com/sportranker/db/DatabaseManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/tuo_database?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "tuo_username";
-    private static final String PASSWORD = "tua_password";
+    private static final String URL = "jdbc:mysql://localhost:3306/sportRankerDB?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "sportranker_user";
+    private static final String PASSWORD = "BHg72612KJo";
 
     private static Connection connection;
 
